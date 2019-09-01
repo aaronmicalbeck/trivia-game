3b2c82bbb325f8e4316d8b2d5df3fab78ecb4dcb
Aaron Beck <aaronmicalbeck@gmail.com>
2019-08-31 21:19:43 -0700

    game working/adding button score values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,33 +2,67 @@
 
 var number = 60;
 var intervalId;
+var score = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // FUNCTIONS //
+$("#start-button").click(makeVisible);
+$("#start-button").click(run);
+$("#submit-button").click(gameOver);
+// ////////////////
+// 
+// TIMER FUNCTIONS
+// 
+// ///////////////
+
+
+// Starts timer and decreases by 1 sec
 
 function run() {
-
+    clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
 }
-
-  function stop() {
-
+function stop() {
     clearInterval(intervalId);
 }
 
+// This function will display the timer in the #show-timer div //
 function decrement() {
     number--;
     $("#show-timer").html("<h2>" + number + "</h2>");
     if (number === 0) {
         stop(); alert("Times Up!");
+        
     }
-   
+
     var converted = timeConverter(number);
-    
+
 
 
     $("#show-timer").text(converted);
 }
 
+// Converts decrement into timer format
 
 function timeConverter(t) {
 
@@ -49,9 +83,64 @@ function timeConverter(t) {
     return minutes + ":" + seconds;
 }
 
+// Clears interval so timer doesn't continue/go negative
 function stop() {
 
     clearInterval(intervalId);
 }
 
-run();
+
+
+// ////////////////
+// 
+// TRIVIA FUNCTIONS
+// 
+// ///////////////
+
+function makeVisible() {
+    $(".container-fluid").css("visibility", "visible")
+};
+function gameOver(){
+    console.log("WORKING!");
+    var score = 0;
+    $(".form-check-input:checked").each(function(){
+        score+=parseInt($(this).val(),10);
+    });
+    console.log(score);
+    $("#yourScore").html(score);
+};
+
+
+    
+    
+
+    
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+// function justTestinStuff(){
+
+//     for (i = 1; i < questions.length; i++) {
+//         let tempVar = document.querySelector(`input[name='inlineRadioOptions${i}'`).value;
+//         console.log(` You selected: ${tempVar} for question ${i}`);
+//     }
+
+// };
+
+
+// document.onkeyup = function(event){
+//     justTestinStuff();
+//     console.log("YOU TESTED STUFF");
+// };
